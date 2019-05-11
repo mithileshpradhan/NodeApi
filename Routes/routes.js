9feafbc135cd436a1b575f8d19672b24e19bfc26
mithileshpradhan <mithi.rocks123@gmail.com>
2019-05-11 23:44:32 +0530

    added post request to the api

--- Routes/routes.js
+++ Routes/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const mongoose = require('mongoose');
 const myModel = require('../Models/models');
 
+
 const URL ="mongodb://myapi:123456myapi@ds155076.mlab.com:55076/myapi"
 mongoose.connect(URL,{ useNewUrlParser: true }).then(() => {
   console.log('Database connection successfull...')
@@ -10,12 +11,30 @@ mongoose.connect(URL,{ useNewUrlParser: true }).then(() => {
 
 //db.once('open', function () {
 
+
+  
+
 router.get('/',(req,res,next) => {
+  
   myModel.find({}).then((doc) => {
     res.json(doc);
   })
 
 })
 
+router.post('/',(req,res,next) => {
+
+  var newUser = new myModel({
+     name:req.body.name,
+     Location:req.body.Location
+     
+  });
+
+  newUser.save().then((doc) => {
+    
+    console.log(doc)
+    
+  })
+})
 
 module.exports = router;
--- api.js
+++ api.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = require('./Routes/routes');
+const bodyParser = require('body-parser');
 
 
 var app = express();
 const port = 4000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/app',router);
 
